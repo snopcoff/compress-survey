b4d27f3d14b60c168175d060099f68dd5cc17089
snopcoff <snoopy.8994@gmail.com>
2017-05-16 07:58:15 +0000

    Finish survey model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :answers
+  has_many :questions, through: :answers
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
