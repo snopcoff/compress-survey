80ded389a84a5c0b1f048abc6a0b16bd9f1aa309
snopcoff <snoopy.8994@gmail.com>
2017-05-16 08:32:06 +0000

    Basic survey view

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answers
   has_many :users, through: :answers
+  
+  accepts_nested_attributes_for :answers
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,5 @@
 class Survey < ActiveRecord::Base
+    has_many :questions
+    
+    accepts_nested_attributes_for :questions
 end
